51a206347609817e0fef018b0042e9d35924f598
Lauri <lauri.teetsov@vikk.ee>
2017-09-21 07:08:41 +0000

    rest of files

---
+++ resources/views/child.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title','Page title')
+
+@section('sidebar')
+    @parent
+    
+    <p>See on sidebar lisa</p>
+@endsection
+
+@section('content')
+    <p>siia tuleb sisu</p>
+@endsection
+
+@component('alert')
+     @slot('title')
+         Keelatud
+     @endslot
+    Puudub juurdepääs 
+@endcomponent
